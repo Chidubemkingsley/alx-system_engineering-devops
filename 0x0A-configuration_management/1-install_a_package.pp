ffa195556a6e4fab8f1185526a19c852c7ad815a
Chidubemkingsley <rubelu2017@gmail.com>
2024-03-22 20:28:18 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Using Puppet to install Flask and Werkzeug with specific versions
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
